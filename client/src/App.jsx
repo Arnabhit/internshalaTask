a64a52c682d79c193099c268a4b29788af9842d8
Arnab Barua <113552756+Arnabhit@users.noreply.github.com>
2024-08-23 01:15:45 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import PokemonCard from './Components/Pokemoncard';
+import PokemonCard from './Components/PokemonCard';
 import './index.css';
 
 const App = () => {
